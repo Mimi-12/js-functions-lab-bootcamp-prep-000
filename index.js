ad50e242f95a160cae0953ba220977480a37396d
selam lemma <oneiconforlife26@gmail.com>
2020-09-08 20:34:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// write your code below!
+function happyHolidays(name) {
+console.log("Happy,holidays!");
+}
+happyHolidays();
+
+var name = "dave"
+function happyHolidaysTo(){
+console.log(`happyHolidaysTo, ${name},${hollyDayName}!`);
+}
+happyHolidaysTo (`happyHolidaysTo, ${dave},${hollyDayName}!`);
+
+var name= "dave"
+var hollyDayName = "christmas"
+function happyCustomHolidayTo(name, hollyDayName) {
+console.log(`happy+ ${name+" " + christmas}`);
+}
+happyCustomHolidayTo (`happy+ ${dave+hollyDayName}`);
+
+var days = 7
+var hollyDayName = "christmas"
+function holidayCountdown(name,hollyDayName){
+cosole.log(`hollyDayName + ${days}`);
+}
+holidayCountdown("dave","christmas");
