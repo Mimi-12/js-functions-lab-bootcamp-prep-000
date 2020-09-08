26357142d6f8e96a13f531888c5f416e411d35a1
selam lemma <oneiconforlife26@gmail.com>
2020-09-08 20:46:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,22 +3,3 @@ def happyHolidays()
 end
 happyHolidays()
 
-var name = "dave"
-function happyHolidaysTo(){
-console.log(`happyHolidaysTo, ${name},${hollyDayName}!`);
-}
-happyHolidaysTo (`happyHolidaysTo, ${dave},${hollyDayName}!`);
-
-var name= "dave"
-var hollyDayName = "christmas"
-function happyCustomHolidayTo(name, hollyDayName) {
-console.log(`happy+ ${name+" " + christmas}`);
-}
-happyCustomHolidayTo (`happy+ ${dave+hollyDayName}`);
-
-var days = 7
-var hollyDayName = "christmas"
-function holidayCountdown(name,hollyDayName){
-cosole.log(`hollyDayName + ${days}`);
-}
-holidayCountdown("dave","christmas");
